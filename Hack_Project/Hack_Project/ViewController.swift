5920f161cf458e8f7fd3a7e72a679b4058b4707e
Михаил Попов <mikhail_popov@MacBook-Air-Mihail.local>
2021-11-26 13:57:36 +0300

    Создал проект/Добавил PinLayout

---
+++ Hack_Project/Hack_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hack_Project
+//
+//  Created by Михаил Попов on 26.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
